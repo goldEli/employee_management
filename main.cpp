79829abcd9face3b842c9edbdad2ad8d5dcde0a9
miaoyu <miaoyu2009@qq.com>
2021-08-07 13:56:22 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test/test.h"
+using namespace std;
+
+int main()
+{
+	int a = 111;
+	cout << "hello" << endl;
+	test(a);
+	return 0;
+};
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+void test(int a)
+{
+	cout << a << endl;
+}
